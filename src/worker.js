e1e14f2f80b97629a8a29d23a1b565748dbb2302
Faizal Chan. <moefc32@gmail.com>
2025-08-28 06:59:21 +0700

    adjust codebase

--- src/worker.js
+++ src/worker.js
@@ -1,19 +1,38 @@
+async function apiFetch(url, options = {}) {
+    const defaultHeaders = {
+        'User-Agent': 'Mfc Site Worker',
+        'Accept': 'application/json',
+    };
+
+    options.headers = { ...defaultHeaders, ...(options.headers || {}) };
+    return fetch(url, options);
+}
+
 export default {
-	async fetch(request, env, ctx) {
-		switch (request.method) {
-			case 'GET':
-				return new Response('Hello World!', {
-					headers: { 'Content-Type': 'text/plain' }
-				});
+    async fetch(request, env, ctx) {
+        switch (request.method) {
+            case 'GET':
+                try {
+                    return new Response('Hello World!', {
+                        headers: { 'Content-Type': 'text/plain' },
+                    });
+                } catch (e) {
+                    return new Response(JSON.stringify({
+                        error: e.message,
+                    }), {
+                        status: 500,
+                        headers: { 'Content-Type': 'application/json' },
+                    });
+                }
 
-			case 'DELETE':
-				return new Response(null, { status: 204 });
+            case 'DELETE':
+                return new Response(null, { status: 204 });
 
-			default:
-				return new Response(JSON.stringify({ error: 'Method Not Allowed' }), {
-					status: 405,
-					headers: { 'Content-Type': 'application/json' }
-				});
-		}
-	},
+            default:
+                return new Response(JSON.stringify({ error: 'Method not allowed' }), {
+                    status: 405,
+                    headers: { 'Content-Type': 'application/json' },
+                });
+        }
+    },
 };
