10497e274a4c2cff102ad1f19ddb1de09925bf58
Faizal Chan. <moefc32@gmail.com>
2025-08-30 04:02:32 +0700

    add the main codes

--- src/worker.js
+++ src/worker.js
@@ -15,13 +15,70 @@ async function apiFetch(url, options = {}) {
 
 export default {
     async fetch(request, env, ctx) {
+        const { searchParams } = new URL(request.url);
+        const searchQuery = searchParams.get('q');
+
+        if (!searchQuery) {
+            return new Response(JSON.stringify({
+                application,
+                message: 'Missing search query string!'
+            }), {
+                status: 400,
+                headers: contentTypeJson,
+            });
+        }
+
         switch (request.method) {
             case 'GET':
                 try {
-                    return new Response('Hello World!', {
-                        headers: {
-                            'Content-Type': 'text/plain',
-                        },
+                    const gcse_cx = env.CONFIG_GCSE_CX;
+                    const gcse_key = env.CONFIG_GCSE_KEY;
+
+                    if (!gcse_cx || !gcse_key) {
+                        return new Response(JSON.stringify({
+                            application,
+                            message: 'Missing environment variable(s)!',
+                        }), {
+                            status: 500,
+                            headers: contentTypeJson,
+                        });
+                    }
+
+                    const response = await apiFetch(
+                        `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(searchQuery)}&cx=${gcse_cx}&key=${gcse_key}&num=10`);
+
+                    if (!response.ok) {
+                        const text = await response.text();
+                        throw new Error(`GCSE API returned ${response.status}: ${text}`);
+                    }
+
+                    const data = await response.json();
+
+                    if (!data.items) {
+                        return new Response(JSON.stringify({
+                            application,
+                            message: 'No search result found.',
+                            data: [],
+                        }), {
+                            status: 200,
+                            headers: contentTypeJson,
+                        });
+                    }
+
+                    const result = data.items?.map((item) => {
+                        return {
+                            title: item.title,
+                            link: item.link,
+                            snippet: item.htmlSnippet,
+                        }
+                    });
+
+                    return new Response(JSON.stringify({
+                        application,
+                        message: 'Fetch data success.',
+                        data: result,
+                    }), {
+                        headers: contentTypeJson,
                     });
                 } catch (e) {
                     return new Response(JSON.stringify({
