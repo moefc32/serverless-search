4879a1f382b11736cd1e48883a02ede34e811089
Faizal Chan. <moefc32@gmail.com>
2025-08-29 19:55:30 +0700

    adjust messages and response structures

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,8 @@
+const application = 'Mfc API';
+const contentTypeJson = {
+    'Content-Type': 'application/json',
+};
+
 async function apiFetch(url, options = {}) {
     const defaultHeaders = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; Win11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36',
@@ -14,14 +19,17 @@ export default {
             case 'GET':
                 try {
                     return new Response('Hello World!', {
-                        headers: { 'Content-Type': 'text/plain' },
+                        headers: {
+                            'Content-Type': 'text/plain',
+                        },
                     });
                 } catch (e) {
                     return new Response(JSON.stringify({
-                        error: e.message,
+                        application,
+                        message: e.message,
                     }), {
                         status: 500,
-                        headers: { 'Content-Type': 'application/json' },
+                        headers: contentTypeJson,
                     });
                 }
 
@@ -29,9 +37,12 @@ export default {
                 return new Response(null, { status: 204 });
 
             default:
-                return new Response(JSON.stringify({ error: 'Method not allowed' }), {
+                return new Response(JSON.stringify({
+                    application,
+                    message: 'Method not allowed!'
+                }), {
                     status: 405,
-                    headers: { 'Content-Type': 'application/json' },
+                    headers: contentTypeJson,
                 });
         }
     },
